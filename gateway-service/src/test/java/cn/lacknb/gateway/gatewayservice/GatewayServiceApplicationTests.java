164404b4f279ddcc67b4ee850e5cc1260ef6e3e4
gitsilence <gitsilence@lacknb.cn>
2023-02-16 10:14:14 +0800

    update

---
+++ gateway-service/src/test/java/cn/lacknb/gateway/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lacknb.gateway.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
